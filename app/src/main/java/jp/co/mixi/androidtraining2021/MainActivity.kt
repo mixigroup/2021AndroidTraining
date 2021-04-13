16b3dc5adec7c4645ffb278fc4d9aa50fefc50b9
Atsuto Yamada <atsuto.yamada@mixi.co.jp>
2021-04-14 00:31:34 +0900

    commit lession1-2 end

--- app/src/main/java/jp/co/mixi/androidtraining2021/MainActivity.kt
+++ app/src/main/java/jp/co/mixi/androidtraining2021/MainActivity.kt
@@ -2,10 +2,43 @@ package jp.co.mixi.androidtraining2021
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Log.d("MixiLesson", "onCreate()")
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        Log.d("MixiLesson", "onStart()")
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        Log.d("MixiLesson", "onResume()")
+    }
+
+    override fun onPause() {
+        super.onPause()
+
+        Log.d("MixiLesson", "onPause()")
+    }
+
+    override fun onStop() {
+        super.onStop()
+
+        Log.d("MixiLesson", "onStop()")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+
+        Log.d("MixiLesson", "onDestroy()")
     }
 }
