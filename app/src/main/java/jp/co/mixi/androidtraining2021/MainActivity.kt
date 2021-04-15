ac0b6e18c1af1f9cd0e1e4aeb80899041eb76578
Atsuto Yamada <atsuto.yamada@mixi.co.jp>
2021-04-15 22:09:41 +0900

    Lesson3-1Start

--- app/src/main/java/jp/co/mixi/androidtraining2021/MainActivity.kt
+++ app/src/main/java/jp/co/mixi/androidtraining2021/MainActivity.kt
@@ -3,42 +3,15 @@ package jp.co.mixi.androidtraining2021
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import androidx.core.widget.addTextChangedListener
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        Log.d("MixiLesson", "onCreate()")
-    }
-
-    override fun onStart() {
-        super.onStart()
-
-        Log.d("MixiLesson", "onStart()")
-    }
-
-    override fun onResume() {
-        super.onResume()
-
-        Log.d("MixiLesson", "onResume()")
-    }
-
-    override fun onPause() {
-        super.onPause()
-
-        Log.d("MixiLesson", "onPause()")
-    }
-
-    override fun onStop() {
-        super.onStop()
-
-        Log.d("MixiLesson", "onStop()")
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-
-        Log.d("MixiLesson", "onDestroy()")
     }
 }
