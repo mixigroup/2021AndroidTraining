3c06bcb199be69d4e05e9567c71b5772332cfc54
Atsuto Yamada <atsuto.yamada@mixi.co.jp>
2021-04-15 23:26:10 +0900

    Lesson4-1End

--- app/src/main/java/jp/co/mixi/androidtraining2021/MainActivity.kt
+++ app/src/main/java/jp/co/mixi/androidtraining2021/MainActivity.kt
@@ -1,16 +1,12 @@
 package jp.co.mixi.androidtraining2021
 
 import android.graphics.Color
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-import android.widget.EditText
 import android.widget.TextView
-import androidx.core.widget.addTextChangedListener
+import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
@@ -19,24 +15,46 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val recyclerView = findViewById<RecyclerView>(R.id.recyclerView)
+        val adapter = MyAdapter()
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        recyclerView.adapter = adapter
+
+        val textList = mutableListOf<String>()
+        for (i in 0..100) {
+            textList.add("要素$i")
+        }
+        adapter.textList = textList
+        adapter.notifyDataSetChanged()
     }
 }
 
 private class MyAdapter : RecyclerView.Adapter<MyViewHolder>() {
 
+    var textList: List<String> = emptyList()
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
-        return TODO()
+        val inflater = LayoutInflater.from(parent.context)
+        val view = inflater.inflate(R.layout.list_item, parent, false)
+        return MyViewHolder(view)
     }
 
     override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
-        TODO()
+        holder.textView.text = textList[position]
+        holder.itemView.setBackgroundColor(
+            if (position % 2 == 0) {
+                Color.WHITE
+            } else {
+                Color.LTGRAY
+            }
+        )
     }
 
     override fun getItemCount(): Int {
-        return TODO()
+        return textList.size
     }
 }
 
 private class MyViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-
+    val textView = itemView.findViewById<TextView>(R.id.textView)
 }
