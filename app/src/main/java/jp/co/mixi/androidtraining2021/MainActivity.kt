3e422a2b735f3ad10e2098325ea9c5ba7cc8652d
Atsuto Yamada <atsuto.yamada@mixi.co.jp>
2021-04-15 23:24:42 +0900

    Lesson4-1start

--- app/src/main/java/jp/co/mixi/androidtraining2021/MainActivity.kt
+++ app/src/main/java/jp/co/mixi/androidtraining2021/MainActivity.kt
@@ -1,22 +1,42 @@
 package jp.co.mixi.androidtraining2021
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 import androidx.core.widget.addTextChangedListener
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val editText = findViewById<EditText>(R.id.editText)
-        editText.addTextChangedListener {
-            val textView = findViewById<TextView>(R.id.textView)
-            textView.text = it.toString()
-        }
     }
+}
+
+private class MyAdapter : RecyclerView.Adapter<MyViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+        return TODO()
+    }
+
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+        TODO()
+    }
+
+    override fun getItemCount(): Int {
+        return TODO()
+    }
+}
+
+private class MyViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
 }
