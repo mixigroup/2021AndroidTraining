97b19fb1445b1ccc8eed5c46321c8b6a8e5a89e8
Atsuto Yamada <atsuto.yamada@mixi.co.jp>
2021-04-16 00:21:06 +0900

    Lesson5-1Start

--- app/src/main/java/jp/co/mixi/androidtraining2021/MainActivity.kt
+++ app/src/main/java/jp/co/mixi/androidtraining2021/MainActivity.kt
@@ -1,68 +1,46 @@
 package jp.co.mixi.androidtraining2021
 
-import android.graphics.Color
+import android.os.AsyncTask
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import android.widget.Button
-import android.widget.EditText
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
+
+    private var task: MyAsyncTask? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val recyclerView = findViewById<RecyclerView>(R.id.recyclerView)
-        val adapter = MyAdapter()
-        recyclerView.layoutManager = LinearLayoutManager(this)
-        recyclerView.adapter = adapter
-
-        val button = findViewById<Button>(R.id.button)
-        button.setOnClickListener {
-            val editText = findViewById<EditText>(R.id.editText)
-            val text = editText.text.toString()
-
-            val newList = adapter.textList.toMutableList()
-            newList.add(text)
-            adapter.textList = newList
-            adapter.notifyDataSetChanged()
-
-            editText.setText("")
+        task = MyAsyncTask().apply {
+            execute(10)
         }
     }
-}
 
-private class MyAdapter : RecyclerView.Adapter<MyViewHolder>() {
-
-    var textList: List<String> = emptyList()
-
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
-        val inflater = LayoutInflater.from(parent.context)
-        val view = inflater.inflate(R.layout.list_item, parent, false)
-        return MyViewHolder(view)
+    override fun onDestroy() {
+        task?.cancel(true)
+        task = null
+        super.onDestroy()
     }
 
-    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
-        holder.textView.text = textList[position]
-        holder.itemView.setBackgroundColor(
-            if (position % 2 == 0) {
-                Color.WHITE
-            } else {
-                Color.LTGRAY
+    private inner class MyAsyncTask : AsyncTask<Int, String, String>() {
+        override fun doInBackground(vararg params: Int?): String {
+            for (i in 1..params[0]!!) {
+                publishProgress("途中経過$i")
+                Thread.sleep(1000)
             }
-        )
-    }
+            return "完了"
+        }
 
-    override fun getItemCount(): Int {
-        return textList.size
-    }
-}
+        override fun onProgressUpdate(vararg values: String?) {
+            val textView = findViewById<TextView>(R.id.textView)
+            textView.text = values[0]
+        }
 
-private class MyViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-    val textView = itemView.findViewById<TextView>(R.id.textView)
+        override fun onPostExecute(result: String?) {
+            val textView = findViewById<TextView>(R.id.textView)
+            textView.text = result
+        }
+    }
 }
