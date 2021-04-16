77b4bfc365dbbdb49cfef1e3d0d82875202c25e1
Atsuto Yamada <atsuto.yamada@mixi.co.jp>
2021-04-16 19:17:02 +0900

    Lesson5-2End

--- app/src/main/java/jp/co/mixi/androidtraining2021/MainActivity.kt
+++ app/src/main/java/jp/co/mixi/androidtraining2021/MainActivity.kt
@@ -1,7 +1,11 @@
 package jp.co.mixi.androidtraining2021
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.lifecycleScope
+import kotlinx.coroutines.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -9,5 +13,30 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val startButton = findViewById<Button>(R.id.startButton)
+        var job: Job? = null
+        startButton.setOnClickListener {
+            if (job?.isActive == true) {
+                return@setOnClickListener
+            }
+            job = lifecycleScope.launch {
+                val textView = findViewById<TextView>(R.id.textView)
+                var count = 0
+                try {
+                    while (true) {
+                        textView.text = "${count}秒"
+                        count++
+                        delay(1000)
+                    }
+                } finally {
+                    textView.text = "停止"
+                }
+            }
+        }
+
+        val stopButton = findViewById<Button>(R.id.stopButton)
+        stopButton.setOnClickListener {
+            job?.cancel()
+        }
     }
 }
